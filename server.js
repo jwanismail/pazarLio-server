2eeef412227a6c8a3251e39f0683b4ee96771b7e
Civan Elismail <157229288+jwanismail@users.noreply.github.com>
2025-05-21 16:12:16 +0300

    CORS finaly

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 
 // CORS ayarları
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://pazarlio.vercel.app', 'https://pazar-lioo.vercel.app'],
+  origin: ['http://localhost:5173', 'https://pazarlio.vercel.app', 'https://pazar-lioo.vercel.app', 'https://pazar-lio-mo4u.vercel.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
