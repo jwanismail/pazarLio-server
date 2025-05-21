c34b93853d33fba8849314fafd115432cf8cc61d
Civan Elismail <157229288+jwanismail@users.noreply.github.com>
2025-05-21 15:51:46 +0300

    CORS ayarları güncellendi

--- server.js
+++ server.js
@@ -9,11 +9,16 @@ const app = express();
 
 // CORS ayarları
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://pazarlio.vercel.app'],
+  origin: ['http://localhost:5173', 'https://pazarlio.vercel.app', 'https://pazar-lioo.vercel.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
+  credentials: true,
+  preflightContinue: false,
+  optionsSuccessStatus: 204
 }));
+
+// Preflight isteklerini yönet
+app.options('*', cors());
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
